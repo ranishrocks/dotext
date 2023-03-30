5a21bc482bb5bed218da45679f0b245d5a97c9b6
Ranish Jamode <ranishjamode@gmail.com>
2023-03-30 21:50:39 +0900

    feat: loading in braille translate
    - loading for clearence

--- lib/ui/pages/translate_cam_page/translate_cam_vm.dart
+++ lib/ui/pages/translate_cam_page/translate_cam_vm.dart
@@ -14,51 +14,59 @@ import '../../../entities/braille_info_page_entity.dart';
 
 class TranslateCamViewModel {
   ///detect object by ML model
-  void runObjectDetection(BuildContext context, ModelObjectDetection _objectModel) async {
-    //pick an image
-    ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+  Future<void> runObjectDetection(BuildContext context, ModelObjectDetection _objectModel) async {
+    try {
+      //pick an image
+      ImagePicker picker = ImagePicker();
+      final XFile? image = await picker.pickImage(source: ImageSource.camera);
 
-    var objDetect = await _objectModel.getImagePrediction(
-      await File(image!.path).readAsBytes(),
-      minimumScore: 0.2,
-      IOUThershold: 0.5
-    );
+      var objDetect = await _objectModel.getImagePrediction(
+        await File(image!.path).readAsBytes(),
+        minimumScore: 0.3,
+        IOUThershold: 0.2
+      );
 
-    String wholeBraille = "";
+      String wholeBraille = "";
 
-    //TODO : change it to sort in two factor
-    objDetect.sort((a, b) {
-      if(a==null || b==null) return 0;
-      return a.rect.left.compareTo(b.rect.left);
-    });
-    objDetect.forEach((element) {
-      print({
-        "score": element?.score,
-        "className": element?.className,
-        "class": element?.classIndex,
-        "rect": {
-          "left": element?.rect.left,
-          "top": element?.rect.top,
-          "width": element?.rect.width,
-          "height": element?.rect.height,
-          "right": element?.rect.right,
-          "bottom": element?.rect.bottom,
-        },
+      //TODO : change it to sort in two factor
+      objDetect.sort((a, b) {
+        if(a==null || b==null) return 0;
+        return a.rect.left.compareTo(b.rect.left);
+      });
+      objDetect.forEach((element) {
+        print({
+          "score": element?.score,
+          "className": element?.className,
+          "class": element?.classIndex,
+          "rect": {
+            "left": element?.rect.left,
+            "top": element?.rect.top,
+            "width": element?.rect.width,
+            "height": element?.rect.height,
+            "right": element?.rect.right,
+            "bottom": element?.rect.bottom,
+          },
+        });
+
+        if(element?.className!=null){
+          int detectedBraille = detectionToCharCode(element?.className as String);
+          if(detectedBraille>0) wholeBraille += String.fromCharCodes([detectedBraille]);
+        }
       });
 
-      if(element?.className!=null){
-        int detectedBraille = detectionToCharCode(element?.className as String);
-        if(detectedBraille>0) wholeBraille += String.fromCharCodes([detectedBraille]);
+      var brailleResult = await SearchApiUseCase().searchBraille(wholeBraille);
+
+      if(brailleResult.runtimeType==List<TextBraillePair>){
+        // ignore: use_build_context_synchronously
+        if((brailleResult as List<TextBraillePair>).isNotEmpty){
+          RouteUtil().push(context, BrailleInfoListPage(textToBrailleList: brailleResult, titleType: TitleType.braille,));
+        }
       }
-    });
 
-    var brailleResult = await SearchApiUseCase().searchBraille(wholeBraille);
-
-    if(brailleResult.runtimeType==List<TextBraillePair>){
-      // ignore: use_build_context_synchronously
-      RouteUtil().push(context, BrailleInfoListPage(textToBrailleList: brailleResult, titleType: TitleType.braille,));
+    } catch (e) {
+      print("error while detecting braille and translating : $e");
     }
+    
   }
 
   ///Change result from braille detection ML to actual unicode(hexa)
